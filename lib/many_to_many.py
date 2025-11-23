e8440b433f9f610df3c425024ae43c8576d0f99e
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-11-23 19:51:56 +0300

    test done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,60 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._contracts = []
+
+    def contracts(self):
+        return self._contracts
+
+    def books(self):
+        return [contract.book for contract in self._contracts]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self._contracts)
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._contracts = []
+
+    def contracts(self):
+        return self._contracts
+
+    def authors(self):
+        return [contract.author for contract in self._contracts]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(book, Book):
+            raise Exception("book must be a Book instance")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an int")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        # Add this contract to author and book contracts list
+        author._contracts.append(self)
+        book._contracts.append(self)
+
+        # Add this contract to the class-level collection
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        filtered_contracts = [contract for contract in cls.all if contract.date == date]
+        return sorted(filtered_contracts, key=lambda c: c.date)
